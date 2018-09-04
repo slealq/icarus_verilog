eb3173d1b8a2ea64aec19378b69b080ca709cb18
Stuart Leal Q <stuart.leal23@gmail.com>
2018-09-04 17:54:13 -0600

    Basic testbench

---
+++ mux_21/mux_tb.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 100ps
+`include "mux.v"
+`include "mux_tester.v"
+
+module mux_tb;
+   wire reset_L, clk, [3:0] data_0, [3:0] data_1, [3:0] data_out;
+
+   mux mux_( .reset_L (reset_L),
+             .clk (clk),
+             .data_0 (data_0),
+             .data_1 (data_1),
+             .data_out (data_out)
+             );
+
+   mux_tester mux_tester_(.reset_L (reset_L),
+                          .clk (clk),
+                          .data_0 (data_0),
+                          .data_1 (data_1),
+                          .data_out (data_out)
+                          );
+endmodule
+
+
