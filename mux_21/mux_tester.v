2e5a72681dfcba536e15d59b51084857e86bf228
Stuart Leal Q <stuart.leal23@gmail.com>
2018-09-04 18:25:48 -0600

    Use correct reset, and counter for change

--- mux_21/mux_tester.v
+++ mux_21/mux_tester.v
@@ -6,28 +6,43 @@ module mux_tester (
                    output reg [3:0] data_1,
                    input [3:0] data_out
                    );
+
+   reg [2:0]                   counter;
+
    initial begin
       $dumpfile("mux.vcd");
       $dumpvars;
       data_0 <= 4'b0;
       data_1 <= 4'b1;
-      reset <= 1'b0;
+      reset_L <= 1'b0;
 
-      // reset flip flops and back to 0
+      // start counter with 0
+      counter <= 'b0;
+
+      // reset_L flip flops and back to 0
       @ (posedge clk);
-      reset <= 1'b1;
+      reset_L <= 1'b1;
 
       // iterate through all values
       repeat (16) begin
          @(posedge clk);
-         data_0 <= data_0 + 1;
-         data_1 <= data_1 - 1;
+         counter <= counter +1;
+
+         if (counter > 'b1) begin
+            data_0 <= data_0 + 1;
+            data_1 <= data_1 - 1;
+            counter <= 'b0;
+         end //if
+
       end
 
       // end with 0 in data values
       @(posedge clk);
       data_0 <= 4'b0;
       data_1 <= 4'b0;
+
+      $finish;
+
    end
 
    initial clk <= 0;
