d5d9719e65ebbc385aa40b02475c22f4fc48cdde
Stuart Leal Q <stuart.leal23@gmail.com>
2018-09-12 23:04:37 -0600

    Add flip flop

--- compuertas/testbench.v
+++ compuertas/testbench.v
@@ -3,7 +3,7 @@
 
 module testbench;
 
-   wire in_not, in1_nand, in2_nand, out_not, out_nand;
+   wire in_not, in1_nand, in2_nand, out_not, out_nand, d, q, clk;
 
    not_delay not_ ( .in (in_not),
                     .out (out_not)
@@ -13,12 +13,20 @@ module testbench;
                         .in1_nand (in1_nand),
                         .in2_nand (in2_nand),
                         .out_nand (out_nand),
-                        .out_not (out_not)
+                        .out_not (out_not),
+                        .D_flop (d),
+                        .Q_flop (q),
+                        .clk (clk)
                         );
 
    nand_delay nand_ (.in1 (in1_nand),
                    .in2 (in2_nand),
                    .out (out_nand)
                    );
+
+   flip_flop_delay flop_ ( .Q (q),
+                           .D (d),
+                           .clk (clk)
+                           );
 
 endmodule
