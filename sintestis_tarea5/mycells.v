f2340c1a14b783e364f7de595f350b76bec53889
Stuart Leal Q <stuart.leal23@gmail.com>
2018-09-23 10:01:16 -0600

    Use delays in standard library

--- sintestis_tarea5/mycells.v
+++ sintestis_tarea5/mycells.v
@@ -1,18 +1,18 @@
 
 module NOT(input A, output Y);
-   assign Y = ~A;
+   assign #(1.5:5.05:7.1) Y = ~A;
 endmodule
 
 module NAND(input A, input B, output Y);
-   assign Y = ~(A & B);
+   assign #(1:3.3:5.1) Y = ~(A & B);
 endmodule
 
 module NOR(input A, input B, output Y);
-   assign Y = ~(A | B);
+   assign #(0.8:2.2:3.6) Y = ~(A | B);
 endmodule
 
 module DFF(input C, input D, output reg Q);
    always @(posedge C)
-     Q <= D;
+     #(1.4:3:4.6) Q <= D;
 endmodule
 
