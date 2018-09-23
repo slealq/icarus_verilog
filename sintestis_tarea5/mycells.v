43f31aefd4977be98fd06cb133a555fa2e793b52
Stuart Leal Q <stuart.leal23@gmail.com>
2018-09-22 20:21:22 -0600

    Correct the library to comply with icarus compiler

--- sintestis_tarea5/mycells.v
+++ sintestis_tarea5/mycells.v
@@ -1,23 +1,18 @@
 
-module NOT(A, Y);
-input A;
-output Y = ~A;
+module NOT(input A, output Y);
+   assign Y = ~A;
 endmodule
 
-module NAND(A, B, Y);
-input A, B;
-output Y = ~(A & B);
+module NAND(input A, input B, output Y);
+   assign Y = ~(A & B);
 endmodule
 
-module NOR(A, B, Y);
-input A, B;
-output Y = ~(A | B);
+module NOR(input A, input B, output Y);
+   assign Y = ~(A | B);
 endmodule
 
-module DFF(C, D, Q);
-input C, D;
-output reg Q;
-always @(posedge C)
-	Q <= D;
+module DFF(input C, input D, output reg Q);
+   always @(posedge C)
+     Q <= D;
 endmodule
 
