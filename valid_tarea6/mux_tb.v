9e95a802170e6cb5a03ae481821bb7dbe59e18be
Stuart Leal Q <stuart.leal23@gmail.com>
2018-09-26 13:55:25 -0600

    Descripción estructural

--- valid_tarea6/mux_tb.v
+++ valid_tarea6/mux_tb.v
@@ -1,35 +1,51 @@
 `timescale 1ns / 100ps
 `include "mux.v"
 `include "mux_tester.v"
+`include "mux_estructural.v"
 
 module mux_tb;
    wire reset_L, clk;
    wire [3:0] data_0;
    wire [3:0] data_1;
-   wire [3:0] data_out;
+   wire [3:0] data_out_cond;
+   wire [3:0] data_out_est;
    wire       valid_0;
    wire       valid_1;
-   wire       valid_out;
+   wire       valid_out_cond;
+   wire       valid_out_est;
 
    mux mux_( .reset_L (reset_L),
              .clk (clk),
              .data_0 (data_0),
              .data_1 (data_1),
-             .data_out (data_out),
+             .data_out (data_out_cond),
              .valid_0 (valid_0),
              .valid_1 (valid_1),
-             .valid_out (valid_out)
+             .valid_out (valid_out_cond)
              );
 
-   mux_tester mux_tester_(.reset_L (reset_L),
-                          .clk (clk),
-                          .data_0 (data_0),
-                          .data_1 (data_1),
-                          .data_out (data_out),
-                          .valid_0 (valid_0),
-                          .valid_1 (valid_1),
-                          .valid_out (valid_out)
-                          );
+   mux_tester mux_tester_( .reset_L (reset_L),
+                           .clk (clk),
+                           .data_0 (data_0),
+                           .data_1 (data_1),
+                           .data_out_cond (data_out_cond),
+                           .data_out_est (data_out_est),
+                           .valid_0 (valid_0),
+                           .valid_1 (valid_1),
+                           .valid_out_cond (valid_out_cond),
+                           .valid_out_est (valid_out_est)
+                           );
+
+   mux_estructural mux_est_( .reset_L (reset_L),
+                             .clk (clk),
+                             .data_0 (data_0),
+                             .data_1 (data_1),
+                             .data_out (data_out_est),
+                             .valid_0 (valid_0),
+                             .valid_1 (valid_1),
+                             .valid_out (valid_out_est)
+                             );
+
 endmodule
 
 
