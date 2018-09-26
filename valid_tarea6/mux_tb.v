c0b9310d8a562b26c526c6d1859d5ab303ac2ce6
Stuart Leal Q <stuart.leal23@gmail.com>
2018-09-26 13:17:13 -0600

    Add valid wires, and connect

--- valid_tarea6/mux_tb.v
+++ valid_tarea6/mux_tb.v
@@ -7,19 +7,28 @@ module mux_tb;
    wire [3:0] data_0;
    wire [3:0] data_1;
    wire [3:0] data_out;
+   wire       valid_0;
+   wire       valid_1;
+   wire       valid_out;
 
    mux mux_( .reset_L (reset_L),
              .clk (clk),
              .data_0 (data_0),
              .data_1 (data_1),
-             .data_out (data_out)
+             .data_out (data_out),
+             .valid_0 (valid_0),
+             .valid_1 (valid_1),
+             .valid_out (valid_out)
              );
 
    mux_tester mux_tester_(.reset_L (reset_L),
                           .clk (clk),
                           .data_0 (data_0),
                           .data_1 (data_1),
-                          .data_out (data_out)
+                          .data_out (data_out),
+                          .valid_0 (valid_0),
+                          .valid_1 (valid_1),
+                          .valid_out (valid_out)
                           );
 endmodule
 
