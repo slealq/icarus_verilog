8c867d4d84664874053693981a8555d63c07fdc4
Stuart Leal Q <stuart.leal23@gmail.com>
2018-09-26 13:17:03 -0600

    Add valid inputs, and logic

--- valid_tarea6/mux.v
+++ valid_tarea6/mux.v
@@ -1,9 +1,12 @@
 // This should be only the conductual description
 module mux (
-            input clk,
-            input reset_L,
-            input [3:0] data_0,
-            input [3:0] data_1,
+            input            clk,
+            input            reset_L,
+            input [3:0]      data_0,
+            input [3:0]      data_1,
+            input            valid_0,
+            input            valid_1,
+            output reg       valid_out,
             output reg [3:0] data_out
             );
    // internal variable
@@ -16,15 +19,22 @@ module mux (
         if (!reset_L) begin
            data_out <= 4'b0;
            selector <= 'b0;
+           valid_out <= 'b0;
         end // if
 
         if (reset_L) begin
            selector <= ~selector;
            if (selector) begin
-              data_out <= data_0;
+              if (valid_0) begin
+                 data_out <= data_0;
+              end
+              valid_out <= valid_0;
               //selector <= 'b0;
            end else begin
-              data_out <= data_1;
+              if (valid_1) begin
+                 data_out <= data_1;
+              end
+              valid_out <= valid_1;
               //selector <= 'b1;
            end
         end // if not reset
