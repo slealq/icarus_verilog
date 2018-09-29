622cfca62bfe55ce1436a9ffc870575cd64afa4e
Stuart Leal Q <stuart.leal23@gmail.com>
2018-09-29 09:22:31 -0600

    Use selector and valid logic as combinational

--- valid_tarea6/mux.v
+++ valid_tarea6/mux.v
@@ -6,21 +6,40 @@ module mux (
             input [3:0]      data_1,
             input            valid_0,
             input            valid_1,
-            output reg       valid_out,
+            output reg valid_out,
             output reg [3:0] data_out
             );
    // internal variable
    reg                       selector;
 
+   initial
+     begin
+        data_out = 0;
+        selector = 0;
+     end
+
+   always @ (*)
+     begin
+        // logic for reset is also combinational
+        if (reset_L) begin
+           if (selector) begin
+              valid_out = valid_0;
+           end else begin
+              valid_out = valid_1;
+           end
+        end else begin
+           valid_out = 0;
+        end
+     end
+
    always @ (posedge clk)
      begin
         // initial values
-
-        if (!reset_L) begin
-           data_out <= 4'b0;
-           selector <= 'b0;
-           valid_out <= 'b0;
-        end // if
+         if (!reset_L) begin
+           // combinational logic
+           data_out <= 0;
+           selector <= 0;
+        end
 
         if (reset_L) begin
            selector <= ~selector;
@@ -28,14 +47,10 @@ module mux (
               if (valid_0) begin
                  data_out <= data_0;
               end
-              valid_out <= valid_0;
-              //selector <= 'b0;
            end else begin
               if (valid_1) begin
                  data_out <= data_1;
               end
-              valid_out <= valid_1;
-              //selector <= 'b1;
            end
         end // if not reset
 
